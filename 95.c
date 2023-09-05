d67d7697b21b6cd3cc824a1ace877fbcfa87ffaf
hyewon669 <144100389+hyewon669@users.noreply.github.com>
2023-09-05 17:50:09 +0900

    Add files via upload

---
+++ 95.c
@@ -0,0 +1,14 @@
+/* 출력연 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+printf("안녕! 씨큐브코딩");
+	
+	
+	return 0;
+}
+*/
---
+++ 별 출력.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main (){
+	
+	printf("		¡Ú\n");
+	printf("	   ¡Ú ¡Ú\n");
+	printf("	 ¡Ú     ¡Ú \n");
+	printf(" ¡Ú ¡Ú	  	  ¡Ú¡Ú\n");
+	printf("	¡Ú		¡Ú\n");
+	printf("	  ¡Ú   ¡Ú\n") ;
+	printf("	¡Ú		¡Ú\n") ;
+	printf(" ¡Ú ¡Ú		  ¡Ú¡Ú\n");
+	printf("	 ¡Ú		¡Ú\n");
+	printf("	   ¡Ú ¡Ú\n");
+	printf("		¡Ú\n");
+	 return 0;
+}
