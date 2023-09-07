1287251e391cb907567bbbe820f4a62c0068adf4
hyewon669 <144100389+hyewon669@users.noreply.github.com>
2023-09-07 18:52:04 +0900

    Add files via upload

---
+++ 9월 7일 3..cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+
+int main()
+{
+	
+	printf( "1.\t2.\t3.\t\n" );
+	printf("CPU\tMemory\tHDD");
+	
+}
---
+++ 9월 7일 5..cpp
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+int main()
+{
+
+	
+	printf( "ABCabc123@#$");
+	printf("\n\n");
+	printf("ABC\babc\b123\b@#$");
+	
+}
