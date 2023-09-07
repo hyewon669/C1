24f446e42d83673603c27c5f633668a0bc038d70
hyewon669 <144100389+hyewon669@users.noreply.github.com>
2023-09-07 18:30:33 +0900

    Add files via upload

---
+++ heart.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("##\n");
+	printf("  ¢½¢½     ¢½¢½\n");
+	printf("¢½¢½¢½¢½  ¢½¢½¢½¢½\n");
+	printf(" ¢½¢½¢½¢½¢½¢½¢½¢½\n");
+	printf("  ¢½¢½¢½¢½¢½¢½¢½\n");
+	printf("   ¢½¢½¢½¢½¢½¢½\n");
+ 	printf("    ¢½¢½¢½¢½¢½\n");
+ 	printf("      ¢½¢½¢½\n");
+ 	printf("        ¢½");
+}
