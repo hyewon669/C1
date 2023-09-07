5acb8905b0ebe1932c0e1a7347f47e61bb3020ec
hyewon669 <144100389+hyewon669@users.noreply.github.com>
2023-09-07 17:38:56 +0900

    Add files via upload

---
+++ 9월7일.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("------\n");
+	printf("|    |\n");
+	printf("|    |\n");
+	printf("|    |\n");
+	printf("------\n");
+	printf("\n");
+	
+	printf("忙式式式式式式式忖\n");
+	printf("弛              弛\n");
+	printf("弛              弛\n");
+	printf("弛              弛\n");
+	printf("弛              弛\n");
+	printf("弛              弛\n"); 
+	printf("弛              弛\n");
+	printf("弛              弛\n");
+	printf("戌式式式式式式式戎\n");
+	
+}
