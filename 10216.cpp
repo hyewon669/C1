310bf7f8857d3f588cae5c904145042ff790e736
hyewon669 <144100389+hyewon669@users.noreply.github.com>
2023-10-21 14:43:04 +0900

    Add files via upload

---
+++ 10216.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int num;
+	scanf("%d",&num);
+	if(num>0)
+	{
+		printf("양수");
+	}
+	else if(num<0)
+	{
+		printf("음수");
+	}
+	
+	else{
+		printf("0");
+	} 
+	 
+}
